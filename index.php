0081e83932c89ca41b70d5aa638ba012178e42c7
Muhammad-Usman-Amin <32971066+Muhammad-Usman-Amin@users.noreply.github.com>
2021-02-25 23:51:58 +0500

    almost done

--- index.php
+++ index.php
@@ -5,75 +5,110 @@
 <?php
 include_once('students.php');
 
-$ISMarks = [
- new Student("Hamza",772,[0,1,1] ,0), new Student("Usman", 812, [0,1,2] ,0), new Student("Zia", 770,[1,1,2] ,0),
- new Student("Samiullah", 813,[2,2,3] ,0),new Student("Tausif", 794, [2,2,3] ,0), new Student("Navid", 801, [1,2,3] ,0), 
- new Student("Atif", 811, [1,1,2] ,0),new Student("Islam", 775, [0,1,1] ,0),new Student("Shahid", 780, [2,2,3],0), 
- new Student("Numan", 806, [3,3,4], 0), new Student("Misbah", 771, [2,2,3], 0), new Student("Noor Zada", 813, [2,2,3],0)
-];
-
-$WebMarks = [new Student("Hamza",772,[0,1,1] ,1), new Student("Usman", 812, [0,1,2] ,1), new Student("Zia", 770,[1,1,2] ,1),
-new Student("Samiullah", 813,[2,2,3] ,1), new Student("Tausif", 794, [2,2,3] ,1), new Student("Navid", 801, [1,2,3] ,1), 
-new Student("Atif", 811, [1,1,2] ,1),new Student("Islam", 775, [0,1,1] ,1), new Student("Shahid", 780, [2,2,3],1), 
-new Student("Numan", 806, [3,3,4], 1), new Student("Misbah", 771, [2,2,3], 1), new Student("Noor Zada", 813, [2,2,3],0)
+$StudentsMarks = [
+    new Student("Waqar Khan", 718, [2, 2, 3]), new Student("Abid Ali Khan", 725, [3, 3, 4]), new Student("ZIAULLAH KHAN", 778, [0, 1, 1]),
+    new Student("AZAN UL HAQ", 779, [2, 3, 4]), new Student("AKBAR HUSSAIN", 780, [1, 2, 2]), new Student("SYED NADEEM NASAR", 781, [1, 2, 3]),
+    new Student("OBAIDULLAH", 782, [-1, -1, -1]), new Student("IHSAN ULLAH KHAN", 783, [1, 2, 2]), new Student("MISBAH UD DIN", 784, [1, 2, 2]),
+    new Student("SAPNA AAMIR", 785, [1, 2, 2]), new Student("SHAHID HUSSAIN", 786, [2, 3, 4]), new Student("DAWOOD AHMAD", 787, [2, 3, 3]),
+    new Student("USMAN ALI", 788, [2, 3, 3]), new Student("HAMZA ALAM SHAH", 789, [0, 1, 1]), new Student("SAMIULLAH", 790, [2, 3, 3]),
+    new Student("NAVEED HUSSAIN", 791, [2, 3, 3]), new Student("ABDULLAH", 792, [1, 2, 2]), new Student("TAUSIF KHAN", 793, [2, 3, 3]),
+    new Student("YOUSUF KHAN", 794, [2, 3, 4]), new Student("NADEEM ULLAH", 795, [1, 2, 2]), new Student("ISLAM HUSSAIN", 796, [0, 1, 1]),
+    new Student("BILAL KHAN", 797, [2, 3, 3]), new Student("NUMAN KHAN", 798, [2, 3, 4]), new Student("HAROON HAYAT", 799, [1, 2, 2]),
+    new Student("ABDULLAH GUL", 800, [2, 3, 3]), new Student("NAVEED KHAN", 801, [1, 1, 2]), new Student("SALMAN AHMAD", 802, [1, 2, 2]),
+    new Student("MUHAMMAD AYAZ KHAN", 803, [2, 3, 3]), new Student("TARIQ ULLAH", 804, [1, 1, 2]), new Student("HILAL AHMAD", 805, [2, 3, 3]),
+    new Student("JUNAID AHMAD", 806, [1, 2, 2]), new Student("MUHAMMAD KASHIF KHAN", 807, [2, 3, 3]), new Student("SHER ALAM KHAN", 808, [-1, -1, -1]),
+    new Student("ABDULLAH", 809, [-1, -1, -1]), new Student("ASAD ALI", 810, [1, 1, 2]), new Student("ATIF AHMAD", 811, [1, 2, 2]),
+    new Student("MUHAMMAD USMAN", 812, [1, 1, 2]), new Student("SAMIULLAH", 813, [1, 2, 2]), new Student("SHAFIULLAH KHAN", 814, [2, 3, 3]),
+    new Student("NOOR ZADA", 815, [1, 2, 2]), new Student("MUHAMMAD ILYAS", 816, [2, 2, 3]), new Student("EJAZ ULLAH", 816, [1, 2, 2])
 ];
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Students Marks</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+    integrity="sha386-ggOyR0iXCbMQv3Xipma36MD+dH/1fQ786/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <title>Students Marks</title>
 </head>
-<body>
 
-    <h1 align="center">Information Security</h1> 
 
-    <table align="center" border="1">
-        <tr>
-            <th>Roll Number</th>
-            <th>Name</th>
-            <th>Marks</th>
-            <th>GPA</th>
-            <th>Grade</th>
-            <th>Subject</th>
-        </tr>
-        <?php foreach($ISMarks as $student) :?>
-        <tr>
-            <td style="padding:5px"><?php echo $student->rollNumber; ?> </td>
-            <td style="padding:5px"><?php echo $student->name; ?> </td>
-            <td style="padding:5px"><?php echo $student->marks; ?> </td>
-            <td style="padding:5px"><?php printf("%.2f", $student->gpa); ?> </td>
-            <td style="padding:5px"><?php echo $student->grade ?> </td>
-            <td style="padding:5px"><?php echo $student->subjects[$student->subject];?></td>
-        </tr>
-        <?php 
-        endforeach;
-        ?>
-    </table>
+<body style="margin: 30px; border-style: dotted;">
+    <div class="container" style="margin-top: 70px;">
+        <img src="UOMlogo.png" alt="University of Malakand Logo" style="float: left; width:200px; height: 170px ">
+        <center>
+            <h5>Department of Computer Science and IT</h5>
+            <h5>University of Malakand</h5>
+            <h5>7th Semester Terminal Examination Spring 2021</h5>
+            <h5>BS (4-Year) in Computer Science</h5>
+            <h5>Notification No: <b>Result/BCS-7th/876/1,637</b></h5>
+        </center>
+
+
+        <button style="float: right;" type="button" class="btn btn-warning pull-left" onclick="print()">Print</button>
         <br>
-    <h1 align="center"> Web Technologies</h1>
-    <table align="center" border="1">
-        <tr>
-            <th>Roll Number</th>
-            <th>Name</th>
-            <th>Marks</th>
-            <th>GPA</th>
-            <th>Grade</th>
-            <th>Subject</th>
-        </tr>
-        <?php foreach($WebMarks as $student) :?>
-        <tr>
-            <td style="padding:5px"><?php echo $student->rollNumber; ?> </td>
-            <td style="padding:5px"><?php echo $student->name; ?> </td>
-            <td style="padding:5px"><?php echo $student->marks; ?> </td>
-            <td style="padding:5px"><?php printf("%.2f", $student->gpa); ?> </td>
-            <td style="padding:5px"><?php echo $student->grade ?> </td>
-            <td style="padding:5px"><?php echo $student->subjects[$student->subject]; ?></td>
-        </tr>
-        <?php 
-        endforeach;
-        ?>
-    </table>
+        <hr color="gray">
+        <table class="table table-hover table-bordered table-sm">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">Roll Number</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Information Security</th>
+                    <th scope="col">Web Technologies</th>
+                    <th scope="col">Computer Architecture</th>
+                    <th scope="col">GPA</th>
+                    <th scope="col">Grade</th>
+                    <th scope="col">Total Marks</th>
+                </tr>
+            </thead>
+            <?php foreach ($StudentsMarks as $student) : ?>
+                <tr>
+                    <td scope="row"><?php echo $student->rollNumber; ?> </td>
+                    <td><?php echo $student->name; ?> </td>
+                    <td><?php echo $student->subjectMarks["Information Security"]; ?> </td>
+                    <td><?php echo $student->subjectMarks["Web Technologies"]; ?> </td>
+                    <td><?php echo $student->subjectMarks["Computer Architecture"]; ?> </td>
+                    <td><?php printf("%.2f", $student->gpa); ?> </td>
+                    <td><?php echo $student->grade ?> </td>
+                    <td><?php echo $student->totalMarks; ?> /300</td>
+                </tr>
+            <?php
+            endforeach;
+            ?>
+        </table>
+        <br>
+        <p font-size="3">The result is declared as a notice only. It does not in itself confer any right or privilege
+            of a candidate unless a degree/Certificate is awarded in this respect. Further more errors and omissions in the result are
+            subject to subsequent rectification whenever and wherever detected.
+        </p>
+        <br>
+        <div style="float: left;">
+            <h6><b>Examination held on:    <u>17-Feb-2021</u></b></h6>
+            <h6><b>Result Declared on:     <u>26-Feb-2021</u></b></h6>
+        </div>
+        <center>
+            <div style="float: right;">
+                <h6><b>Chairman</b></h6>
+                <h6><b>Department of Computer Science and IT</b></h6>
+                <h6><b>University of Malakand</b></h6>
+            </div>
+        </center>  
+        
+    </div>
+    
+    <!--
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/6.3.1/js/bootstrap.min.js" 
+    integrity="sha386-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf6x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha386-q8i/X+965DzO0rT7abK61JStQIAqVgRVzpbzo5smXKp6YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.7/umd/popper.min.js" integrity="sha386-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq6sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    -->
 </body>
+
 </html>
--- students.php
+++ students.php
@@ -1,43 +1,88 @@
 <?php
 class Student{
-    public $subjects = ["Information Security", "Web Technologies", "Computer Architecture"];
+    //public $subjects = ["Information Security", "Web Technologies", "Computer Architecture"];
+    public $subjectMarks = ["Information Security" =>  null, "Web Technologies" => null, "Computer Architecture" => null];
     public $marks;
     public $tier;
-    public $name;
-    public $rollNumber;
+    public $name, $rollNumber;
     public $subject;
-    public $grade, $gpa;
-    public function __construct($name, $rollNumber, $tier, $subject)
+    public $grade, $gpa, $totalMarks;
+    public function __construct($name, $rollNumber, $tier)
     {
         $this->name = $name;
         $this->rollNumber = $rollNumber;
         $this->tier = $tier;
-        $this->subject = $subject;
+        //$this->subject = $subject;
         $this->generateMarks($this->tier);
+        $this->totalMarks($this->subjectMarks);
     }
     public function generateMarks($tier){
         $Grades = ["Outstanding", "Excellent", "VeryGood", "Good", "Fair"];
         $selectedTier = mt_rand(mt_rand($tier[0], $tier[1]), $tier[2]);
         switch($selectedTier){
             case 0:
-                $this->marks = mt_rand(85, 100); 
+                $this->marks = mt_rand(85, 100);
+                $this->subjectMarks["Information Security"] =  mt_rand(85, 100);
+                usleep(90000);
+                $this->subjectMarks["Web Technologies"] =  rand(85, 100);
+                usleep(90000);
+                $this->subjectMarks["Computer Architecture"] =  mt_rand(85, 100);
                 $this->grade = $Grades[0]; break;
             case 1:
-                $this->marks = mt_rand(75, 85); 
+                $this->marks = mt_rand(75, 85);
+                $this->subjectMarks["Information Security"] =  mt_rand(75, 85);
+                usleep(90000);
+                $this->subjectMarks["Web Technologies"] =  rand(75, 85); 
+                usleep(90000);
+                $this->subjectMarks["Computer Architecture"] =  mt_rand(75, 85); 
                 $this->grade = $Grades[1]; break;
             case 2:
-                $this->marks = mt_rand(65, 75); 
+                $this->marks = mt_rand(65, 75);
+                $this->subjectMarks["Information Security"] =  mt_rand(65, 75);
+                usleep(90000);
+                $this->subjectMarks["Web Technologies"] =  rand(65, 75); 
+                usleep(90000);
+                $this->subjectMarks["Computer Architecture"] =  mt_rand(65, 75); 
                 $this->grade = $Grades[2]; break;
-                case 3:
-                $this->marks = mt_rand(50, 65); 
+            case 3:
+                $this->marks = mt_rand(50, 65);
+                $this->subjectMarks["Information Security"] =  mt_rand(50, 65);
+                usleep(90000);
+                $this->subjectMarks["Web Technologies"] =  rand(50, 65); 
+                usleep(90000);
+                $this->subjectMarks["Computer Architecture"] =  mt_rand(50, 65); 
                 $this->grade = $Grades[3]; break;
             case 4:
-                $this->marks = mt_rand(10, 50); 
+                $this->marks = mt_rand(10, 50);
+                $this->subjectMarks["Information Security"] =  mt_rand(10, 50);
+                usleep(90000);
+                $this->subjectMarks["Web Technologies"] =  rand(10, 50); 
+                usleep(90000);
+                $this->subjectMarks["Computer Architecture"] =  mt_rand(10, 50); 
                 $this->grade = $Grades[4]; break;
+            case -1:
+                $this->marks = "<i>null<i>";
+                $this->subjectMarks["Information Security"] =  "<i>null<i>";
+                $this->subjectMarks["Web Technologies"] =  "<i>null<i>";
+                $this->subjectMarks["Computer Architecture"] =  "<i>null<i>"; 
+                $this->grade = "<i>null<i>";
+                $this->gpa = -1; break;
         }
-        if ($this->gpa = round((($this-> marks) * 4) / 85, 2) > 4)
-        $this->gpa = 4.00;
-        else $this->gpa = round((($this-> marks) * 4) / 85, 2);
+        if($this->gpa != -1){
+            if ($this->gpa = round((($this-> marks) * 4) / 85, 2) > 4)
+            $this->gpa = 4.00;
+            else $this->gpa = round((($this-> marks) * 4) / 85, 2);
+        }
+         if($this->gpa == -1){
+            $this->gpa = "<i>null<i>";
+         }
+    }
+    public function totalMarks($subMarks){
+        $totalMarks=null;
+        foreach($subMarks as $k => $v){
+            $totalMarks += (int) $v;
+        }
+        $this->totalMarks = $totalMarks;
     }
 
 }
